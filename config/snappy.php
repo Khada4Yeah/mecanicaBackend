0876a763a651178de322e84b2ad984352e926e6c
KhadaYeah <pespinoza9494@utm.edu.ec>
2024-06-27 01:36:41 -0500

    Error por ruta

--- config/snappy.php
+++ config/snappy.php
@@ -43,14 +43,14 @@ return [
         "env" => [],
     ],
 
-    "image" => [
-        "enabled" => true,
-        "binary" => env(
-            "WKHTML_IMG_BINARY",
-            '"C:\Program Files\wkhtmltopdf\bin\wkhtmltoimage"',
-        ),
-        "timeout" => false,
-        "options" => [],
-        "env" => [],
-    ],
+    // "image" => [
+    //     "enabled" => true,
+    //     "binary" => env(
+    //         "WKHTML_IMG_BINARY",
+    //         '"C:\Program Files\wkhtmltopdf\bin\wkhtmltoimage"',
+    //     ),
+    //     "timeout" => false,
+    //     "options" => [],
+    //     "env" => [],
+    // ],
 ];
