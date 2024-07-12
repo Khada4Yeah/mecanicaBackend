c74fdea7e1537fce0d96cb2a3ab1a0b0fa59f0bf
KhadaYeah <pespinoza9494@utm.edu.ec>
2024-07-12 10:22:09 -0500

    agg fuente con archivo css

--- resources/views/ficha.blade.php
+++ resources/views/ficha.blade.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-
+    <link rel="stylesheet" href="{{ public_path('fonts/stylesheet.css') }}">
     <style>
         body {
-            font-family: sans-serif;
+            font-family: 'Arial', sans-serif;
             position: relative;
         }
 
